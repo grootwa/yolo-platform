5ddcd49be5cb0e164cf2dfcefe3c648723e2e23c
bikash <rsavn01@gmail.com>
2026-04-12 23:57:31 +0530

    db connection checked

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI
+from motor.motor_asyncio import AsyncIOMotorClient
 
 app = FastAPI()
+
+# connect db 
+
+MONGO_DETAILS = "mongodb://db:27017"
+
+# create a client
+client =  AsyncIOMotorClient(MONGO_DETAILS)
+
+database = client.yolo_db
 
 @app.get("/")
 def home():
     return{"message": "hello world !"}
+
+@app.get("/home")
+async def home():
+    try:
+        await client.admin.command('ping')
+        status = "Database Connected"
+    except Exception as e:
+        status = f"Database connection failed : {e}"
+    
+    return {"db status": status}
